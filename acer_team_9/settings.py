277d44f414b4b15f6770c6c7521668fed9cc4448
ATHZoA79 <106025017anthonyhsu@gmail.com>
2024-02-04 13:19:15 +0800

    update settings.py

--- acer_team_9/settings.py
+++ acer_team_9/settings.py
@@ -17,7 +17,7 @@ import django_heroku
 
 django_heroku.settings(locals())
 
-
+PROJECT_DIR = os.environ.get("PROJECT_DIR", "acerteam9")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
