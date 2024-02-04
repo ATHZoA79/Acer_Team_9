d72964644952fba4e01a5cd6676af8cf7973b3a2
ATHZoA79 <106025017anthonyhsu@gmail.com>
2024-02-04 12:56:05 +0800

    delete staticfile dir. update settings.py file

--- acer_team_9/settings.py
+++ acer_team_9/settings.py
@@ -157,7 +157,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_ROOT = BASE_DIR / "staticfile"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
