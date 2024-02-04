0323b910989c9aa9c07eb008537b451fb3a35885
ATHZoA79 <106025017anthonyhsu@gmail.com>
2024-02-04 16:39:33 +0800

    update settings.py

--- acer_team_9/settings.py
+++ acer_team_9/settings.py
@@ -112,7 +112,6 @@ WSGI_APPLICATION = "acer_team_9.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
@@ -120,6 +119,7 @@ DATABASES = {
     }
 }
 DB_DIR = os.path.join(BASE_DIR, "db.sqlite3")
+DATABASES["default"] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
@@ -155,7 +155,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "staticfiles"),
