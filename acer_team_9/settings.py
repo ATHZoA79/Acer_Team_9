e3215c4f0dd8e6688b10f15f12025d0517d3853f
ATHZoA79 <106025017anthonyhsu@gmail.com>
2024-02-04 13:53:18 +0800

    run collectstatic command locally

--- acer_team_9/settings.py
+++ acer_team_9/settings.py
@@ -155,11 +155,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATIC_URL = "/static/"
+STATIC_URL = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, "static", "image"),
-    os.path.join(BASE_DIR, "static", "css"),
+    os.path.join(BASE_DIR, "staticfiles"),
+    os.path.join(BASE_DIR, "staticfiles", "image"),
+    os.path.join(BASE_DIR, "staticfiles", "css"),
 ]
 
 # Default primary key field type
