d7032ce33668fdb7f77ec42e553bec6956cc2530
ATHZoA79 <106025017anthonyhsu@gmail.com>
2024-02-10 04:55:38 +0800

    update admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
+from .models import ScheduleModel, PlanModel
+
 
 # Register your models here.
+@admin.register(ScheduleModel)
+class RequestDemoAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in ScheduleModel._meta.get_fields()]
+
+
+@admin.register(PlanModel)
+class RequestDemoAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in PlanModel._meta.get_fields()]
